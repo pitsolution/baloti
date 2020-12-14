c8ee7d4776720017f87f07a176869427d8569fff
jpic <jpic@yourlabs.org>
2020-12-14 11:36:09 +0000

    Added automatic deployment for master.electeez.com and electeez.com

--- electeez/settings.py
+++ electeez/settings.py
@@ -1,20 +1,19 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$zglx0*+-v3t^unyl!porw)-l!kw3-9&m68wvei^knt!gyoex-'
+SECRET_KEY = os.getenv('SECRET_KEY', 'notsecret')
+SECURE = SECRET_KEY != 'notsecret'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if SECURE:
+    DEBUG = False
+    ALLOWED_HOSTS = [os.getenv('HOST')]
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = []
 
 REGISTRATION_OPEN = True
 ACCOUNT_ACTIVATION_DAYS = 7
@@ -103,11 +102,16 @@ WSGI_APPLICATION = 'electeez.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'PASSWORD': os.getenv('DB_PASS', None),
+        'HOST': os.getenv('DB_HOST', None),
+        'USER': os.getenv('DB_USER', None),
+        'PORT': os.getenv('DB_PORT', None),
     }
 }
 
+MEMCACHED_HOST = os.getenv('MEMCACHED_HOST', 'localhost')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -146,7 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'public'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 EMAIL_HOST = os.getenv('EMAIL_HOST', None)
 EMAIL_PORT = os.getenv('EMAIL_PORT', None)
@@ -154,7 +160,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', None)
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', None)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', None)
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', None)
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@electeez.io')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@electeez.com')
 
 if EMAIL_HOST:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
