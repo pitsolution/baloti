113d7777897c31a6126ed582a2ead5c1bd45661b
jpic <jpic@yourlabs.org>
2021-01-12 14:09:58 +0100

    Fix user admin

--- electeez_auth/admin.py
+++ electeez_auth/admin.py
@@ -3,4 +3,18 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 UserAdmin.ordering = None
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('first_name', 'last_name', 'email')
+UserAdmin.fieldsets = (
+    (None, {'fields': ('email', 'password',)}),
+    ('Personal info', {'fields': ('first_name', 'last_name')}),
+    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    ('Important dates', {'fields': ('last_login', 'date_joined')}),
+)
+UserAdmin.add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('username', 'password1', 'password2'),
+    }),
+)
 admin.site.register(User, UserAdmin)
