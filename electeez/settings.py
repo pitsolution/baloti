148a36fb6eeb17e7d4bc7e4a1b933dace2e18d47
tmig <tmig@yourlabs.org>
2021-06-01 19:07:48 +0200

    change db backend

--- electeez/settings.py
+++ electeez/settings.py
@@ -200,8 +200,8 @@ WSGI_APPLICATION = 'electeez.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'electeez'),
         'PASSWORD': os.getenv('DB_PASS', None),
         'HOST': os.getenv('DB_HOST', None),
         'USER': os.getenv('DB_USER', None),
