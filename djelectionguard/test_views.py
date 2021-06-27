a5f7ca3e8f069145b8f87d25e1b6cf046e546302
jpic <jpic@yourlabs.org>
2021-06-27 22:00:08 +0200

    Add missing publish test
    Without 15bef5f, this test fails just like in production::
    
        ERROR    django.request:log.py:224 Internal Server Error: /en/contest/6899fb1f-17ca-429e-a8e0-ff32e7031d5b/publish/
        Traceback (most recent call last):
          File "/home/jpic/.local/lib/python3.9/site-packages/django/core/handlers/exception.py", line 47, in inner
            response = get_response(request)
          File "/home/jpic/.local/lib/python3.9/site-packages/django/core/handlers/base.py", line 181, in _get_response
            response = wrapped_callback(request, *callback_args, **callback_kwargs)
          File "/home/jpic/.local/lib/python3.9/site-packages/django/contrib/auth/decorators.py", line 21, in _wrapped_view
            return view_func(request, *args, **kwargs)
          File "/home/jpic/.local/lib/python3.9/site-packages/django/views/generic/base.py", line 70, in view
            return self.dispatch(request, *args, **kwargs)
          File "/home/jpic/.local/lib/python3.9/site-packages/django/views/generic/base.py", line 98, in dispatch
            return handler(request, *args, **kwargs)
          File "/home/jpic/.local/lib/python3.9/site-packages/django/views/generic/edit.py", line 194, in post
            return super().post(request, *args, **kwargs)
          File "/home/jpic/.local/lib/python3.9/site-packages/django/views/generic/edit.py", line 142, in post
            return self.form_valid(form)
          File "/home/jpic/src/electeez/djelectionguard/views.py", line 451, in form_valid
            self.object.publish()
          File "/home/jpic/src/electeez/djelectionguard/models.py", line 207, in publish
            from electionguard.tally import publish_ciphertext_tally
        ImportError: cannot import name 'publish_ciphertext_tally' from 'electionguard.tally'
    
    This will prevent new EG releases that break the publish code from
    being deployed.

--- djelectionguard/test_views.py
+++ djelectionguard/test_views.py
@@ -506,3 +506,9 @@ vot1@example.com\rvot2@example.com\rnew@example.com
     _client = Client()
     assert _client.post(link)['Location'] == contest_url
     assert _client.get(contest_url).status_code == 200
+
+    # test publish works
+    response = post(mediator, f'{contest_url}publish/')
+    assert response.status_code == 302
+    contest.refresh_from_db()
+    assert contest.artifacts_sha1, 'contest publish fail'
