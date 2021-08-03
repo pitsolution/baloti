b3089006aab2a086935d47aa46380deb2ac4c125
tmig <tmig@yourlabs.org>
2021-08-03 10:47:43 +0200

    user can create config on the fly

--- electeez_sites/utils.py
+++ electeez_sites/utils.py
@@ -1,19 +1,33 @@
+from functools import wraps
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 
 from .models import Site
 
 
-def create_access_required(*args, **kwargs):
+def site_passes_test(test_func):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_decorator(view_func):
+            if test_func(request.user, Site.get_current()):
+                return login_required(view_func)
+            return staff_member_required(view_func)
+        return _wrapped_decorator
+    return decorator
+
+
+def create_access_required(function):
     try:
         Site.objects.clear_cache()
         site = Site.objects.get_current()
     except Exception:
         return staff_member_required(*args, **kwargs)
 
-    if site.all_users_can_create:
-        return login_required(*args, **kwargs)
-    return staff_member_required(*args, **kwargs)
+    actual_decorator = site_passes_test(
+        lambda s: s.all_users_can_create
+    )
+
+    return actual_decorator(function)
 
 
 def result_access_required(*args, **kwargs):
