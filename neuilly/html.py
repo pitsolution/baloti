820f593e1ece23231a8579ec8fb1026daea8358c
tmig <tmig@yourlabs.org>
2021-09-17 16:55:12 +0200

    data privacy

--- neuilly/html.py
+++ neuilly/html.py
@@ -289,9 +289,24 @@ class Legal(Div):
                         text_align='center'
                     )
                 ),
-                Div(mark_safe(
-                    escape(_('DATA_PRIVACY_TEXT'))
-                ).replace('\n', '<br>')),
+                Div(
+                    mark_safe('''<!-- OneTrust Privacy Notice start -->
+  <!-- Container in which the privacy notice will be rendered -->
+  <div id="otnotice-e2599a30-e42d-4991-8e52-c386265763b7" class="otnotice"></div>
+
+  <script src="https://privacyportalde-cdn.onetrust.com/privacy-notice-scripts/otnotice-1.0.min.js" type="text/javascript" charset="UTF-8" id="otprivacy-notice-script">
+      settings="eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyJ9"
+    </script>
+
+  <script type="text/javascript" charset="UTF-8">
+      // To ensure external settings are loaded, use the Initialized promise:
+      OneTrust.NoticeApi.Initialized.then(function() {
+        OneTrust.NoticeApi.LoadNotices(["https://privacyportalde-cdn.onetrust.com/40ec4ddd-1627-4efb-b428-a53e211552ad/privacy-notices/e2599a30-e42d-4991-8e52-c386265763b7.json"]);
+      });
+    </script>
+
+  <!-- OneTrust Privacy Notice end -->'''),
+                ),
                 cls='info-panel',
             )
         )
