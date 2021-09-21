81a2276255b004d3fcda01a916528cbe7f80aedf
tmig <tmig@yourlabs.org>
2021-09-21 20:17:15 +0200

    remove svg in emails

--- electeez_sites/admin.py
+++ electeez_sites/admin.py
@@ -1,24 +1,12 @@
 from django import forms
 from django.contrib import admin
-from django_svg_image_form_field import SvgAndImageFormField
 
 
 from .models import Site
 
 
-class SiteForm(forms.ModelForm):
-    class Meta:
-        model = Site
-        exclude = []
-        field_classes = {
-            'email_banner': SvgAndImageFormField,
-            'email_footer': SvgAndImageFormField,
-        }
-
-
 @admin.register(Site)
 class SiteAdmin(admin.ModelAdmin):
-    form = SiteForm
     list_display = ('domain', 'name')
     search_fields = ('domain', 'name')
 
