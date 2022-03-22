0c2543310e5ee6b1d55c0020bbc3f4e5f98df6e4
Jibi John <jibi.jn@pitsolutions.com>
2022-03-22 16:35:44 +0530

    Registration flow changes and modal

--- baloti_auth/admin.py
+++ baloti_auth/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BalotiUser
 
-# Register your models here.
+class BalotiUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'password',
+        'status'
+    )
+
+admin.site.register(BalotiUser, BalotiUserAdmin)
--- baloti_auth/models.py
+++ baloti_auth/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class BalotiUser(models.Model):
+    STATUS = (
+        ('draft', 'DRAFT'),
+        ('done', 'DONE')
+    )
+
+    username = models.CharField(max_length=63)
+    password = models.CharField(max_length=63)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS,
+        default='draft',
+        null=True
+    )
+
+    def __str__(self):
+        return self.username
