b818820b7aa4c91ba3fa67f0b3d17bf5cbf46d69
Jibi John <jibi.jn@pitsolutions.com>
2022-05-10 12:45:36 +0530

    Commands for translation of existing data

--- baloti_djelectionguard/cron_jobs.py
+++ baloti_djelectionguard/cron_jobs.py
@@ -1,9 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from djelectionguard.models import ParentContest, Contest, Recommender, ContestType, Initiator
+from djelectionguard.models import ParentContest, Contest
 from datetime import datetime
 from django.utils import timezone
-from deep_translator import GoogleTranslator
-from djlang.models import Language
 sched = BackgroundScheduler()
 sched.start()
 
@@ -22,89 +20,6 @@ def close_votes():
 
     return True
 
-def parent_contest_autotranslate():
-
-    queryset = ParentContest.objects.all()
-    from .models import ParentContesti18n
-    for each in queryset:
-        for language in Language.objects.all():
-            translated_name = GoogleTranslator('auto', language.iso).translate(each.name)
-            parentcontest = ParentContesti18n.objects.filter(parent_contest_id=each.pk,language=language)
-            if not parentcontest:
-                ParentContesti18n.objects.create(parent_contest_id=each,language=language,name= translated_name)
-
-    return True
-
-def contest_autotranslate():
-
-    queryset = Contest.objects.all()
-    from .models import Contesti18n
-    for each in queryset:
-        for language in Language.objects.all():
-            contest = Contesti18n.objects.filter(contest_id=each.pk,language=language)
-            if not contest:
-                translated_name = GoogleTranslator('auto', language.iso).translate(each.name)
-                translated_about = GoogleTranslator('auto', language.iso).translate(each.about)
-                translated_against = GoogleTranslator('auto', language.iso).translate(each.against_arguments)
-                translated_infavour = GoogleTranslator('auto', language.iso).translate(each.infavour_arguments)
-                Contesti18n.objects.create(contest_id=each, parent=each.parent, language=language,
-                    name= translated_name, against_arguments=translated_against, about=translated_about,
-                    infavour_arguments=translated_infavour)
-
-    return True
-
-def Recommenderautotranslate():
-
-    queryset = Recommender.objects.all()
-    from .models import Recommenderi18n
-    for each in queryset:
-        for language in Language.objects.all():
-            recommender = Recommenderi18n.objects.filter(recommender_id=each.pk,language=language)
-            if not recommender:
-                translated_name = GoogleTranslator('auto', language.iso).translate(each.name)
-                translated_type = GoogleTranslator('auto', language.iso).translate(each.recommender_type)
-                Recommenderi18n.objects.create(recommender_id=each,language=language,name= translated_name, recommender_type=translated_type)
-
-    return True
-
-def ContestTypeautotranslate():
-
-    queryset = ContestType.objects.all()
-    from .models import ContestTypei18n
-    for each in queryset:
-        for language in Language.objects.all():
-            contesttype = ContestTypei18n.objects.filter(contest_type_id=each.pk,language=language)
-            if not contesttype:
-                translated_name = GoogleTranslator('auto', language.iso).translate(each.name)
-                ContestTypei18n.objects.create(contest_type_id=each,language=language,name= translated_name)
-
-    return True
-
-def Initiatorautotranslate():
-
-    queryset = Initiator.objects.all()
-    languages = Language.objects.all()
-    from .models import Initiatori18n
-    print('queryset==============================', queryset)
-    for each in queryset:
-        print('languages============================', languages)
-        for language in languages:
-            initiator = Initiatori18n.objects.filter(initiator_id=each.pk, language=language)
-            print('initiator=============================', initiator)
-            if not initiator:
-                print('test--------------------------------------------')
-                translated_name = GoogleTranslator('auto', language.iso).translate(each.name)
-                Initiatori18n.objects.create(initiator_id=each, language=language, name= translated_name)
-
-    return True
-
-def daily_cron():
-    # parent_contest_autotranslate()
-    # contest_autotranslate()
-    # Recommenderautotranslate()
-    # ContestTypeautotranslate()
-    Initiatorautotranslate()
-
 def minute_cron():
     close_votes()
 
@@ -112,4 +27,3 @@ def initialize_cron():
     """Initializes daily crons
     """
     sched.add_job(minute_cron, trigger='cron', start_date='2022-03-29', day_of_week='mon-sun', minute='*')
-    sched.add_job(daily_cron, trigger='cron', start_date='2022-05-09', day_of_week='mon-sun', hour=3, minute=20)
---
+++ baloti_djelectionguard/management/commands/translation.py
@@ -0,0 +1,14 @@
+from baloti_djelectionguard.utils.translation import parent_contest_autotranslate, contest_autotranslate, \
+                        Recommenderautotranslate, ContestTypeautotranslate, Initiatorautotranslate
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Runs elastic search indexing."
+
+    def handle(self, *args, **options):
+        print('test----------------------------')
+        parent_contest_autotranslate()
+        # contest_autotranslate()
+        Recommenderautotranslate()
+        ContestTypeautotranslate()
+        Initiatorautotranslate()
