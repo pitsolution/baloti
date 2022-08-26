406b0ea80cfbd7a7db8d86343497752c88060302
Jibi John <jibi.jn@pitsolutions.com>
2022-08-26 18:23:58 +0530

    [Task] Language code and abbreviation

--- baloti_djelectionguard/templatetags/base_tags.py
+++ baloti_djelectionguard/templatetags/base_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from electeez_common.settings import BASE_URL
+import json
+import ast
 
 register = template.Library()
 
@@ -9,3 +11,21 @@ def getOGImage():
     thumbnail_url = BASE_URL + '/static/images/thumbnail.png'
     print('thumbnail_url======================', thumbnail_url)
     return thumbnail_url
+
+@register.simple_tag
+def displayLanguageCode(code):
+    with open("baloti_djelectionguard/static/languages.json") as data_file:
+        myJson = data_file.read()
+        myJson = ast.literal_eval(myJson)
+        if code in myJson:
+            code = myJson[code][0]['display_code']
+    return code
+
+@register.simple_tag
+def displayLanguageName(code, name):
+    with open("baloti_djelectionguard/static/languages.json") as data_file:
+        myJson = data_file.read()
+        myJson = ast.literal_eval(myJson)
+        if code in myJson:
+            name = myJson[code][0]['display_name']
+    return name
